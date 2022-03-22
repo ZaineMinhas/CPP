352e6a41d8643832e67d6a0128f6c0d658f4b488
ZaineMinhas <zaineminhas5@gmail.com>
2022-03-22 14:52:15 +0100

    on commence le module_02

---
+++ module_02/ex00/Fixed.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zminhas <zminhas@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/22 14:47:22 by zminhas           #+#    #+#             */
+/*   Updated: 2022/03/22 14:49:40 by zminhas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+# include <string>
+
+class Fixed
+{
+private:
+
+public:
+	Fixed();
+	~Fixed();
+};
+
+#endif
