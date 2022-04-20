7f61bbbfab94c733cc72e9029794d95db71b9968
ZaineMinhas <zaineminhas5@gmail.com>
2022-04-20 19:36:52 +0200

    ex01 a continuer

---
+++ module_05/ex01/Form.cpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zminhas <zminhas@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/20 19:05:48 by zminhas           #+#    #+#             */
+/*   Updated: 2022/04/20 19:36:20 by zminhas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Form.hpp"
+
+Form::Form() : _name("NONE"), _grade(150), _grade_to_exec(0), _signed(0) {}
+
+Form::Form(std::string name, int grade) : _name(name), _grade(grade), _grade_to_exec(0) {
+	if (_grade < 1)
+		throw (GradeTooHighException());
+	else if (_grade > 150)
+		throw (GradeTooLowException());
+}
+
+Form::Form(Form const &src) : _name(src._name), _grade(src._grade), _grade_to_exec(0), _signed(src._signed) {}
+
+Form::~Form() {}
+
+Form	Form::operator=(Form const &rhs) {
+	*this = rhs;
+}
+
+std::string const	Form::getName(void) const {
+	return (this->_name);
+}
+
+int const	Form::getGrade(void) const {
+	return (this->_grade);
+}
+
+void	Form::beSigned(Bureaucrat &bureaucrat) {
+	a continuer ^^
+}
+
+std::ostream	&operator<<(std::ostream &os, Form const &lhs) {
+	os << lhs.getName() << ", bureaucrat grade " << lhs.getGrade();
+	return (os);
+}
--- module_05/ex01/Form.hpp
+++ module_05/ex01/Form.hpp
@@ -6,13 +6,50 @@
 /*   By: zminhas <zminhas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 18:50:29 by zminhas           #+#    #+#             */
-/*   Updated: 2022/04/19 18:51:03 by zminhas          ###   ########.fr       */
+/*   Updated: 2022/04/20 19:28:44 by zminhas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FORM_HPP
 # define FORM_HPP
 
-# include <iostream>
+# include "Bureaucrat.hpp"
+
+class Form
+{
+private:
+	std::string const	_name;
+	bool				_signed;
+	int const			_grade;
+	int const			_grade_to_exec;
+		
+public:
+	Form();
+	Form(std::string name, int grade);
+	Form(Form const &src);
+	~Form();
+	Form	operator=(Form const &rhs);
+
+	std::string const	getName(void) const;
+	int const			getGrade(void) const;
+
+	void	beSigned(Bureaucrat &bureaucrat);
+
+	class GradeTooHighException : public std::exception {
+	public:
+		virtual const char		*what() const throw() {
+			return ("garde is too high");
+		}
+	};
+
+	class GradeTooLowException : public std::exception {
+	public:
+		virtual const char		*what() const throw() {
+			return ("garde is too low");
+		}
+	};
+};
+
+std::ostream	&operator<<(std::ostream &os, Form const &rhs);
 
 #endif
