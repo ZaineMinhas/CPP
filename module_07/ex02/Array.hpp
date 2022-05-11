d19bb0539849c9129159b9136ca4c4d280555c8a
ZaineMinhas <zaineminhas5@gmail.com>
2022-05-11 03:17:27 +0200

    fin du module_07

--- module_07/ex02/Array.hpp
+++ module_07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: zminhas <zminhas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 04:06:21 by zminhas           #+#    #+#             */
-/*   Updated: 2022/05/10 04:13:16 by zminhas          ###   ########.fr       */
+/*   Updated: 2022/05/11 02:04:11 by zminhas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,37 @@
 # define ARRAY_HPP
 
 # include <iostream>
+# include <exception>
 
 template<typename T>
 class Array
 {
-	Array<T>(void);
-	~Array<T>(void);
+private:
+	T	*_array;
+	const unsigned int _size;
+
+public:
+	Array(void) : _size(0) { this->_array = new T[0]; }
+	Array(const unsigned int n) : _size(n) { this->_array = new T[n](); }
+	Array(const Array<T> &src) { *this = src; }
+	~Array(void) { delete [] this->_array; }
+
+	Array	&operator=(const Array &rhs) {
+		delete this->_array;
+		this->_array = new T[rhs.size()];
+		for (unsigned int i = 0; i < rhs.size(); i++)
+			this->_array[i] = rhs[i];
+		return (*this);
+	}
+
+	T	&operator[](const unsigned int index) {
+		if (index < 0 || index > this->_size)
+			throw (std::out_of_range("index out of range"));
+		return (this->_array[index]);
+	}
+
+	T				*getArray(void) const { return (this->_array); }
+	unsigned int	size(void) const { return (this->_size); }
 };
 
 #endif
--- module_07/ex02/main.cpp
+++ module_07/ex02/main.cpp
@@ -6,9 +6,55 @@
 /*   By: zminhas <zminhas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 04:07:10 by zminhas           #+#    #+#             */
-/*   Updated: 2022/05/10 04:07:20 by zminhas          ###   ########.fr       */
+/*   Updated: 2022/05/11 03:16:44 by zminhas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
 
+int	main(void)
+{
+	Array<int>			IntArray(4);
+	Array<char>			CharArray(6);
+	Array<std::string>	StrArray(2);
+
+	try
+	{
+		CharArray[10] = 'A';
+	}
+	catch (std::exception &e){
+		std::cout << e.what() << std::endl;
+	}
+	
+	std::cout << std::endl << "--------------------------" << std::endl << std::endl;
+	
+	size_t size = IntArray.size();
+	for (size_t i = 0; i < size; i++)
+		IntArray[i] = 0;
+	IntArray[1] = 42;
+	for (size_t i = 0; i < size; i++)
+		std::cout << "IntArray[" << i << "] : " << IntArray[i] << " | ";
+	std::cout << std::endl;
+
+	std::cout << std::endl << "--------------------------" << std::endl << std::endl;
+
+	size = CharArray.size();
+	for (size_t i = 0; i < size; i++)
+		CharArray[i] = 'a';
+	CharArray[3] = 'A';
+	for (size_t i = 0; i < size; i++)
+		std::cout << "CharArray[" << i << "] : " << CharArray[i] << " | ";
+	std::cout << std::endl;
+
+	std::cout << std::endl << "--------------------------" << std::endl << std::endl;
+
+	size = StrArray.size();
+	for (size_t i = 0; i < size; i++)
+		StrArray[i] = "Coucou";
+	StrArray[1] = "Hello";
+	for (size_t i = 0; i < size; i++)
+		std::cout << "StrArray[" << i << "] : " << StrArray[i] << " | ";
+	std::cout << std::endl;
+
+	return (0);
+}
